72026e8194beb5ead78f647cacbd178737256171
Quby <malay77patra@gmail.com>
2026-06-24 18:15:51 +0530

    google auth fix

--- src/auth.js
+++ src/auth.js
@@ -12,6 +12,10 @@ export const auth = betterAuth({
         github: {
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        },
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }
     }
 });
